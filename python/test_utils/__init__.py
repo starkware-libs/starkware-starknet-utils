f87bc0a395202324d0f6253434e0a6476c9f2a9a
RoeeGross <88274280+RoeeGross@users.noreply.github.com>
2025-07-23 23:40:38 +0300

    feat: python utils (#93)

---
+++ python/test_utils/__init__.py
@@ -0,0 +1,2 @@
+# Automatically load shared pytest fixtures from this package
+pytest_plugins = ["test_utils.conftest"]
---
+++ python/test_utils/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+from python.test_utils.starknet_test_utils import StarknetTestUtils
+from typing import Iterator
+import time
+
+
+@pytest.fixture(scope="function")
+def starknet_test_utils() -> Iterator[StarknetTestUtils]:
+    with StarknetTestUtils.context_manager() as val:
+        # TODO: replace the sleep with await.
+        time.sleep(2)
+        yield val
